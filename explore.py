8f560a64020703119aaa58c6593cf122d6e37df6
Arackal Tejas <arackaltejas@Arackals-MacBook-Air.local>
2019-05-19 13:00:33 -0400

    created a new package for protocols/methods related to trading and stocks. delegated the stock_info method to the package.

--- explore.py
+++ explore.py
@@ -1,22 +1,6 @@
-import datetime as dt
+from trade.stock import stock_info
 import matplotlib.pyplot as plt
 import matplotlib.style as style
-import pandas as pd
-import pandas_datareader.data as web
-from pathlib import Path
-from utility.timer import timer
-
-
-@timer
-def stock_info(tikr, year):
-
-    if Path(f'trade-data/{tikr}_{year}').is_file():
-        df = pd.read_csv(f'trade-data/{tikr}_{year}', parse_dates=True, index_col=0)
-    else:
-        df = web.DataReader(tikr, 'yahoo', dt.datetime(year,1,1), dt.datetime(year,12,31))
-        df.to_csv(f'trade-data/{tikr}_{year}')
-
-    return df
 
 
 if __name__ == '__main__':
@@ -24,5 +8,4 @@ if __name__ == '__main__':
 
     tsla_stock = stock_info('TSLA', 2018)
 
-    tsla_stock.plot()
     plt.show()
