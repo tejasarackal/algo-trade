11c70ddfdec7264a92ba87ac2871445d6ad66578
Arackal Tejas <arackaltejas@Arackals-MacBook-Air.local>
2019-05-30 01:54:43 -0400

    labels added to functions to account for the outcome column in dataframe. New function reshape_features_from_list added to convert list into a numpy array and reshape it to be used with sklearn models.

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -2,11 +2,17 @@ import numpy as np
 from sklearn import preprocessing
 
 
-def build_features(stock_df):
-    x = np.array(stock_df.drop('label', 1))
+def build_features(stock_df, label):
+    x = np.array(stock_df.drop(label, 1))
     preprocessing.scale(x)  # pre-processing to scale the outcomes
     return x
 
 
-def outcome(stock_df):
-    return np.array(stock_df['label'])
+def outcome(stock_df, label):
+    return np.array(stock_df[label])
+
+
+def reshape_features_from_list(list_: list):
+    features = np.array([[feature] for feature in list_])
+    features = features.reshape(len(features), -1)
+    return features
